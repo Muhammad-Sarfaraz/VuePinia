22f0c0ec2c8c27cfb0c4aaf45e5d13f9dab51c2d
id.sent.me@gmail.com <id.sent.me@gmail.com>
2023-03-31 21:42:12 +0600

    wip

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,43 @@
+<template>
+    <label
+      v-if="label"
+      :for="uuid"
+    >
+      {{ label }}
+    </label>
+    <input
+      class="field"
+      v-bind="{
+        ...$attrs,
+        onInput: updateValue
+      }"
+      :id="uuid"
+      :value="modelValue"
+      :placeholder="label"
+      :aria-describedby="error ? `${uuid}-error` : null"
+      :aria-invalid="error ? true : false"
+      :class="{ error }"
+    >
+  </template>
+  
+  <script>
+  
+  export default {
+    props: {
+      label: {
+        type: String,
+        default: ''
+      },
+      modelValue: {
+        type: [String, Number],
+        default: ''
+      }
+    },
+    methods:{
+        updateValue(event){
+            this.$emit('update:modelValue', event.target.value);
+        }
+    }
+  }
+  </script>
+  
---
+++ src/components/NoticeBoard.vue
@@ -0,0 +1,22 @@
+<template>
+
+    <h1>{{ notice }}</h1>
+
+  <button @click="close">X</button>
+
+</template>
+
+<script>
+
+export default{
+    props:{
+        notice:String,
+    },
+    methods:{
+        close(){
+            this.$emit('button-clicked');
+        }
+    }
+}
+
+</script>
