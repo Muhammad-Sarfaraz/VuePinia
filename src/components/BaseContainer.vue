fe17b82e3028631c6f61df2e608e1f24a1d9f08b
id.sent.me@gmail.com <id.sent.me@gmail.com>
2023-04-03 22:29:19 +0600

    wip

---
+++ src/components/BaseContainer.vue
@@ -0,0 +1,51 @@
+<template>
+    <h6>Base Container</h6>
+</template>
+
+<script>
+export default {
+  setup(){
+    const userStore = useUserStore()
+    return {
+      userStore,
+      avatar: userStore.avatar,
+    }
+  },
+  components: {
+    Box,
+    Reset,
+    Fire,
+  },
+  data() {
+    return {
+      avatar:'',
+      score: 0,
+      data: {
+        array: [
+          {
+            id: 1,
+            name: "Box A",
+          },
+          {
+            id: 2,
+            name: "Box B",
+          },
+          {
+            id: 3,
+            name: "Box C",
+          },
+        ],
+      },
+    };
+  },
+  methods: {
+    handleFire(value) {
+      this.score = parseInt(value);
+    },
+  },
+
+  created(){
+    this.avatar = this.userStore.avatar;
+  }
+};
+</script>
